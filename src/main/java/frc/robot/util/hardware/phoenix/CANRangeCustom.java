bc95ceefb14461a8f4c55758ec636e08f52f5397
BredPerson <155865812+BredPerson@users.noreply.github.com>
2025-04-08 17:39:28 -0700

    I thinks it is done :) (don't quote me on that)

--- src/main/java/frc/robot/util/hardware/phoenix/CANRangeCustom.java
+++ src/main/java/frc/robot/util/hardware/phoenix/CANRangeCustom.java
@@ -8,7 +8,11 @@ import com.ctre.phoenix6.StatusCode;
 import com.ctre.phoenix6.StatusSignal;
 import com.ctre.phoenix6.configs.CANrangeConfiguration;
 import com.ctre.phoenix6.configs.CANrangeConfigurator;
+import com.ctre.phoenix6.configs.FovParamsConfigs;
+import com.ctre.phoenix6.configs.ProximityParamsConfigs;
+import com.ctre.phoenix6.configs.ToFParamsConfigs;
 import com.ctre.phoenix6.hardware.CANrange;
+import com.ctre.phoenix6.signals.UpdateModeValue;
 
 import edu.wpi.first.units.measure.Distance;
 import frc.robot.util.Constants.CANRangeConstants;
@@ -16,6 +20,10 @@ import frc.robot.util.Constants.FieldConstants;
 import frc.robot.util.Constants.GeneralHardwareConstants;
 
 public class CANRangeCustom extends CANrange {
+
+    private final ProximityParamsConfigs proxParamsConfigs = new ProximityParamsConfigs();
+    private final FovParamsConfigs FOVParamsConfigs = new FovParamsConfigs();
+    private final ToFParamsConfigs tofParamsConfigs = new ToFParamsConfigs();
 
     private final CANrangeConfigurator configurator = getConfigurator();
     private double timeoutSeconds = GeneralHardwareConstants.TIMEOUT_SECONDS;
@@ -133,4 +141,69 @@ public class CANRangeCustom extends CANrange {
         return BaseStatusSignal.refreshAll(distance);
     }
 
+    /**
+     * Configures the paramaters of the FOV in which things are detected.
+     * 
+     * @param CenterX The center of the FOV on the x plane.
+     * @param CenterY The center of the FOV on the y plane.
+     * @param RangeX The range at which the FOV spans on the x plane.
+     * @param RangeY The range at which the FOV spans on the y plane.
+    */
+    public StatusCode configFOVParams(double CenterX, double CenterY, double RangeX, double RangeY) {
+        FOVParamsConfigs.FOVCenterX = CenterX;
+        FOVParamsConfigs.FOVCenterY = CenterY;
+        FOVParamsConfigs.FOVRangeX = RangeX;
+        FOVParamsConfigs.FOVRangeY = RangeY;
+        return 
+        applyParameter(
+            () -> configurator.apply(FOVParamsConfigs, timeoutSeconds),
+            () -> configurator.refresh(FOVParamsConfigs,timeoutSeconds), 
+            () -> FOVParamsConfigs.FOVCenterX == CenterX 
+                && FOVParamsConfigs.FOVCenterY == CenterY 
+                && FOVParamsConfigs.FOVRangeX == RangeX
+                && FOVParamsConfigs.FOVRangeY == RangeY, 
+            "Fov Params"
+        );
+    }
+
+    /**
+     * Configures the paramaters of the proximity at which things are detected.
+     * 
+     * @param minValid The minimum signal strength for a valid measurement.
+     * @param hysteresis Used to account for dips above and below the set threshold.
+     * @param threshold Threshold at which an object will be detected.
+     */
+    public StatusCode configProxParams(double minValid, double hysteresis, double threshold) {
+        proxParamsConfigs.MinSignalStrengthForValidMeasurement = minValid;
+        proxParamsConfigs.ProximityHysteresis = hysteresis;
+        proxParamsConfigs.ProximityThreshold = threshold;
+        return 
+        applyParameter(
+            () -> configurator.apply(proxParamsConfigs,timeoutSeconds),
+            () -> configurator.refresh(proxParamsConfigs,timeoutSeconds),
+            () -> proxParamsConfigs.MinSignalStrengthForValidMeasurement == minValid 
+                && proxParamsConfigs.ProximityHysteresis == hysteresis 
+                && proxParamsConfigs.ProximityThreshold == threshold,
+            "Proximity Params"
+        );
+    }
+
+    /**
+     * Time of Flight paramaters.
+     * 
+     * @param frequency The update frequency.
+     * @param updateModeValue The set update mode either LongRangeUserFreq, ShortRange100Hz, or ShortRangeUserFreq.
+    */
+    public StatusCode configToFParams(double frequency, UpdateModeValue updateModeValue) {
+        tofParamsConfigs.UpdateFrequency = frequency;
+        tofParamsConfigs.UpdateMode = updateModeValue;
+        return 
+        applyParameter(
+            () -> configurator.apply(tofParamsConfigs, timeoutSeconds),
+            () -> configurator.refresh(tofParamsConfigs, timeoutSeconds),
+            () -> tofParamsConfigs.UpdateFrequency == frequency
+                && tofParamsConfigs.UpdateMode == updateModeValue,
+            "ToF Params"
+        );
+    }
 }
